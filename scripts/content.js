2d0b712bf99d243cf462e57e87de305a52bf3d2d
crissdot <crissdotdev@gmail.com>
2024-07-29 01:23:54 +0000

    WIP add listeners to video player

--- scripts/content.js
+++ scripts/content.js
@@ -1,1 +1,39 @@
-console.log('IT WORKS!')
+let isFirstTime = true;
+let saveTimeInLSInterval;
+
+function waitForVideoElement() {
+  const observer = new MutationObserver((mutations, me) => {
+    const player = document.getElementsByTagName('video')[0];
+    if (player) {
+      addVideoPlayerListeners(player);
+      me.disconnect();
+      return;
+    }
+  });
+
+  observer.observe(document, {
+    childList: true,
+    subtree: true
+  });
+}
+
+function addVideoPlayerListeners(player) {
+  player.addEventListener('play', () => {
+    if (isFirstTime) {
+      console.log('Setting initial time');
+      player.currentTime = 8000;
+      isFirstTime = false;
+    }
+
+    saveTimeInLSInterval = setInterval(() => {
+      console.log('time', player.currentTime);
+    }, 10000);
+  });
+
+  player.addEventListener('pause', () => {
+    console.log('Video paused', saveTimeInLSInterval);
+    clearInterval(saveTimeInLSInterval);
+  });
+}
+
+waitForVideoElement();
